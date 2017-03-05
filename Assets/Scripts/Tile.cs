ce01ae0f09ecb220c30092fc5294abae9c8efb8c
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-03-05 13:03:31 -0800

    Revert "Initial Commit"
    This reverts commit f3e28b9e19b86e4ecd312326672d2868bb2f2e7b.

--- Assets/Scripts/Tile.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tile : MonoBehaviour
-{
-    public bool isWalkable;
-    public bool blocksProjectiles;
-    public float movementCost = 1;
-}
